a88ac7af09ecf0107ffb83a2090b8c8dd93231aa
Liron Borwick-Cooper <Liron.Borwick-Cooper@viasat.com>
2020-08-06 08:50:02 -0700

    added gitlab yml

---
+++ tests/FirstTest.js
@@ -0,0 +1,121 @@
+/**
+ * Script Name: {first-test}
+ * 
+ * Generated using  New Relic Synthetics Formatter for Katalon
+ *
+ * Feel free to explore, or check out the full documentation
+ * https://docs.newrelic.com/docs/synthetics/new-relic-synthetics/scripting-monitors/writing-scripted-browsers
+ * for details.
+ */
+
+/** CONFIGURATIONS **/
+
+// Theshold for duration of entire script - fails test if script lasts longer than X (in ms)
+var ScriptTimeout = 180000;
+// Script-wide timeout for all wait and waitAndFind functions (in ms)
+var DefaultTimeout = 30000;
+// Change to any User Agent you want to use.
+// Leave as "default" or empty to use the Synthetics default.
+var UserAgent = "default";
+
+/** HELPER VARIABLES AND FUNCTIONS **/
+
+const assert = require('assert'),
+	By = $driver.By,
+	browser = $browser.manage()
+/** BEGINNING OF SCRIPT **/
+
+console.log('Starting synthetics script: {first-test}');
+console.log('Default timeout is set to ' + (DefaultTimeout/1000) + ' seconds');
+
+// Setting User Agent is not then-able, so we do this first (if defined and not default)
+if (UserAgent && (0 !== UserAgent.trim().length) && (UserAgent != 'default')) {
+  $browser.addHeader('User-Agent', UserAgent);
+  console.log('Setting User-Agent to ' + UserAgent);
+}
+
+// Get browser capabilities and do nothing with it, so that we start with a then-able command
+$browser.getCapabilities().then(function () { })
+	.then(() => {
+            logger.log(1, "https://agitated-lewin-f45196.netlify.app/");
+            return $browser.get("https://agitated-lewin-f45196.netlify.app/");
+        })
+	.then(() => {
+            logger.log(2, "clickElement //div[@id=\'root\']/button");
+            return $browser.waitForAndFindElement(By.xpath("//div[@id='root']/button"), DefaultTimeout)
+                .then(function (el) {
+                    el.click();
+                })
+        })
+	.then(function() {
+		logger.end();
+		console.log('Browser script execution SUCCEEDED.');
+	}, function(err) {
+		logger.end();
+		console.log ('Browser script execution FAILED.');
+		throw(err);
+	});
+
+
+//** Export Functions
+const logger=(function (timeout=3000, mode='production') {
+
+    var startTime = Date.now(),
+        stepStartTime = Date.now(),
+        prevMsg = '',
+        prevStep = 0;
+
+
+    if (typeof $util == 'undefined'  ){
+        $util = {
+            insights: {
+                set: (msg) => {
+                    console.log(`dryRun: sending to Insights using ${msg}`)
+                }
+            }
+        }
+
+    }
+
+    function log(thisStep, thisMsg) {
+
+        if (thisStep > prevStep && prevStep != 0) {
+            end()
+        }
+
+        stepStartTime = Date.now() - startTime;
+
+        if (mode != "production") {
+            stepStartTime = 0
+
+        }
+
+        console.log(`Step ${thisStep}: ${thisMsg} STARTED at ${stepStartTime}ms.`);
+
+        prevMsg = thisMsg;
+        prevStep = thisStep;
+
+    }
+
+    function end() {
+        var totalTimeElapsed = Date.now() - startTime;
+        var prevStepTimeElapsed = totalTimeElapsed - stepStartTime;
+
+        if (mode != 'production') {
+            prevStepTimeElapsed = 0
+            totalTimeElapsed = 0
+        }
+
+        console.log(`Step ${prevStep}: ${prevMsg} FINISHED. It took ${prevStepTimeElapsed}ms to complete.`);
+
+        $util.insights.set(`Step ${prevStep}: ${prevMsg}`, prevStepTimeElapsed);
+        if (timeout > 0 && totalTimeElapsed > timeout) {
+            throw new Error('Script timed out. ' + totalTimeElapsed + 'ms is longer than script timeout threshold of ' + timeout + 'ms.');
+        }
+    }
+
+    return {
+        log,
+        end
+    }
+})(ScriptTimeout)
